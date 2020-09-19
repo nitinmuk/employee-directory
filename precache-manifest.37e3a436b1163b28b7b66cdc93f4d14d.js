9d5c00d25df26712026e28db75c862a2a2da6410
Nitin Mukesh <nitinmukes@gmail.com>
2020-09-19 14:20:58 +0800

    Updates

---
+++ precache-manifest.37e3a436b1163b28b7b66cdc93f4d14d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0efe42c1907d97b217f6cd59ed2d9851",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "3190c290ec5ee8e81216",
+    "url": "/employee-directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8e8973e9ebd8641d87b5",
+    "url": "/employee-directory/static/css/main.61bfcb68.chunk.css"
+  },
+  {
+    "revision": "3190c290ec5ee8e81216",
+    "url": "/employee-directory/static/js/2.67cb75f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.67cb75f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8973e9ebd8641d87b5",
+    "url": "/employee-directory/static/js/main.a252d44c.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9aea39d683ae9f7b935048596d10e779.js"
+  "/employee-directory/precache-manifest.37e3a436b1163b28b7b66cdc93f4d14d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/employee-directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
